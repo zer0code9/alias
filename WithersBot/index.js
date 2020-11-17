36c80a2511d5e0e8de0dd6d9c400272265af295d
WithersWolrd <asorinus@penguin.lxd>
2020-11-17 15:02:46 -0800

    Deleted somethings

--- WithersBot/index.js
+++ WithersBot/index.js
@@ -1,14 +1,8 @@
-const fs = require("fs");
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-
 const token = "NzY4MjE0Njk2MDE5ODg2MTIx.X49NsA.LxdzcdiJLcF22qqDk9Uii2E-fJE";
 const prefix = "z!";
-
-
-// CONFIGS
-var botting = token.startsWith("mfa.");
 
 bot.on("ready", () => {
     bot.user.setPresence({
