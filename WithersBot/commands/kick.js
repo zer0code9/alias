7c7480c53e029d564c09e2e27cfdd8fad16d51de
WithersWolrd <asorinus@penguin.lxd>
2020-11-20 17:26:12 -0800

    made changes

--- WithersBot/commands/kick.js
+++ WithersBot/commands/kick.js
@@ -1,11 +1,13 @@
 const { prefix } = require("/home/asorinus/workspace/myFirstProject/WithersWorld/WithersBot/config.json");
 function kickUser(msg, args) {
+    if (msg.author != guild.owner) {msg.channel.send(`You are not the owner, ${msg.author}`)} else {
     if (msg.mentions.users.size) {
         const taggedUser = msg.mentions.users.first();
         msg.channel.send(`${taggedUser.username} has been successfully kicked.\n\ Only use "kick" when someone has a bad behavior`);
       } else {
         msg.reply(`I don't know that person`);
       }
+}
 }
 
 module.exports = {
