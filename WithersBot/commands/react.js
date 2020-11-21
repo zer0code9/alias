961973691c99118729e4df82eae7f88d9262cd0e
WithersWolrd <asorinus@penguin.lxd>
2020-11-20 18:08:59 -0800

    changed react to avatar

--- WithersBot/commands/react.js
+++ WithersBot/commands/react.js
@@ -1,8 +1,18 @@
+const { prefix } = require("/home/asorinus/workspace/myFirstProject/WithersWorld/WithersBot/config.json");
+
 module.exports = {
-    name: "react",
-    description: "Add reactions to words!",
+    name: "avatar",
+    description: "See the avatars of others!",
     example: "still in progress",
-    execute(msg, args, server){
-        msg.react("😁");
+    execute(msg, args){
+        if (!msg.mentions.users.size) {
+			return msg.channel.send(`Your avatar: <${msg.author.displayAvatarURL}>`);
+		}
+
+		const avatarList = msg.mentions.users.map(user => {
+			return `${user.username}'s avatar: <${user.displayAvatarURL}>`;
+		});
+
+		msg.channel.send(avatarList);
     }
 }
