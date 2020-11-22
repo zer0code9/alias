b74613e7e35cb18d048a1346fcf74690d590b06f
WithersWolrd <asorinus@penguin.lxd>
2020-11-21 22:26:38 -0800

    new command

--- WithersBot/commands/channelInfo.js
+++ WithersBot/commands/channelInfo.js
@@ -1,16 +1,17 @@
 const { prefix } = require("/home/asorinus/workspace/myFirstProject/WithersWorld/WithersBot/config.json");
 const Discord = require("discord.js");
 function cInfo(msg, args) {
-    const channel = msg.channels.get("name", `${args}`);
+    const channel = msg.guild.roles.cache.find(role => role.name === `${args}`);
     const channelInfo = new Discord.MessageEmbed()
     .setColor('RANDOM')
     .setTitle(`Info on`)
     .setDescription('**WithersBot Commands:**')
     .addFields(
-        { name: "Id", value: `${channel.id}` }
+        { name: "Id", value: channel.id }
     )
     .setFooter('The Bot of WithersWorld')
-msg.channel.send(`${channelInfo}`);
+msg.channel.send(channelInfo);
+msg.channel.send(channel)
 }
 module.exports = {
     name: "channel",
