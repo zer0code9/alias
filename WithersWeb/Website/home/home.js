e8bd34f15c53cdff8c916fe912fddb53c92bc6a3
WithersWolrd <asorinus@penguin.lxd>
2020-12-12 11:47:01 -0800

    changes

---
+++ WithersWeb/Website/home/home.js
@@ -0,0 +1,25 @@
+  window.onload = function () {
+    lax.init()
+
+    lax.addDriver('scrollY', function () {
+      return window.scrollY
+    })
+
+    lax.addElements('#findEase', {
+        scrollY: {
+            translateX: [
+            ["elInY", "elCenterY", "elOutY"],
+            [0, 'screenWidth/2', 'screenWidth'],
+            ]
+        }
+    })
+    lax.addElements('#homeWelcome', {
+        scrollY: {
+            translateY: [
+                ["elInX"],
+                [0, 'elHeight/2','elHeight'],
+                ["easeInOutQuad"],
+            ]
+        }
+    })
+  }
