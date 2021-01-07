dc6b439a443ad75df2aff0cf1c28112ac14516b0
WithersWolrd <asorinus@penguin.lxd>
2021-01-06 19:14:58 -0800

    changes

--- WithersBot/commands/pokemon.js
+++ WithersBot/commands/pokemon.js
@@ -1,27 +1,29 @@
 const { prefix } = require("/home/asorinus/workspace/myFirstProject/WithersWorld/WithersBot/config.json");
 const Discord = require("discord.js");
+//const pokeAPI = require('https://pokeapi.co/api/v2/pokemon/')
 function abc(msg, args) {
-    const pokemon = args.join(" ");
+    const pokename = args.join(" ");
     if (pokemon) {
+        //const pokemon = pokeAPI + pokename;
         const poke = new Discord.MessageEmbed()
         .setColor("RANDOM")
         .setTitle("WithersBot Commands")
-        .setDescription("Command: ")
+        .setDescription("Command: pokemon")
         .addFields(
-            { name: "here", value: `here`}
+            { name: "Name", value: `hi`}
         )
         .setFooter("WithersBot helps")
         msg.channel.send(poke);
     } else {
-        const no = new Discord.MessageEmbed()
+        const noPokemon = new Discord.MessageEmbed()
         .setColor("RANDOM")
         .setTitle("WithersBot Commands")
-        .setDescription("Command: ")
+        .setDescription("Command: pokemon")
         .addFields(
-            { name: "Command", value: `here\n\`\`\`${prefix}\`\`\``}
+            { name: "Command", value: `Get info on any pokemon\n\`\`\`${prefix}pokemon [pokemon]\`\`\``}
         )
         .setFooter("WithersBot helps")
-        msg.channel.send(no);
+        msg.channel.send(noPokemon);
     }
 }
 
