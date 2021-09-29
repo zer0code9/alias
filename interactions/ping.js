c54a2407e4f0854f592bbf9dbfad70c103f3eab1
WithersWorld <asorinus@gmail.com>
2021-09-28 20:06:05 -0700

    new

--- interactions/ping.js
+++ interactions/ping.js
@@ -1,5 +1,8 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
-    name: "ping",
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with Pong!'),
 	async execute(interaction) {
 		return interaction.reply('Pong!');
 	},
